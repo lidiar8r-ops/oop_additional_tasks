527b1259074cb542ad697f63614c62e7f00aa31e
Рыбина Лидия <lidiar@rambler.ru>
2025-11-20 20:11:58 +0500

    home work _ lesson 3 _ farction

--- lesson3/fraction.py
+++ lesson3/fraction.py
@@ -10,10 +10,73 @@
 
 
 class Fraction:
-    pass
+    def __init__(self, numerator, denominator):
+        if denominator == 0:
+            raise ValueError("Denominator cannot be zero")
+
+        self.numerator = numerator
+        self.denominator = denominator
+        self.simplify()
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.numerator}, {self.denominator})'
+
+    def __str__(self):
+        return f"{self.numerator} / {self.denominator}"
+
+    def __add__(self, other):
+        new_numerator = (self.numerator * other.denominator +
+                         other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __sub__(self, other):
+        new_numerator = (self.numerator * other.denominator -
+                         other.numerator * self.denominator)
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __mul__(self, other):
+        new_numerator = self.numerator * other.numerator
+        new_denominator = self.denominator * other.denominator
+        return Fraction(new_numerator, new_denominator)
+
+    def __truediv__(self, other):
+        if other.numerator == 0:
+            raise ValueError("Cannot divide by zero fraction")
+        new_numerator = self.numerator * other.denominator
+        new_denominator = self.denominator * other.numerator
+        return Fraction(new_numerator, new_denominator)
+
+    def __eq__(self, other):
+        return (self.numerator == other.numerator and
+                self.denominator == other.denominator)
+
+    def __lt__(self, other):
+        return (self.numerator * other.denominator <
+                other.numerator * self.denominator)
+
+    def gcd(self, a, b):
+        while b != 0:
+            a, b = b, a % b
+        return a
+
+    def simplify(self):
+        # Обработка знака: делаем знаменатель всегда положительным
+        if self.denominator < 0:
+            self.numerator = -self.numerator
+            self.denominator = -self.denominator
+
+        common_divisor = self.gcd(abs(self.numerator), abs(self.denominator))
+        self.numerator //= common_divisor
+        self.denominator //= common_divisor
+
+        # Если числитель 0, делаем знаменатель 1
+        if self.numerator == 0:
+            self.denominator = 1
 
 
-# код для проверки 
+# код для проверки
 fraction1 = Fraction(1, 2)
 print(repr(fraction1))  # Fraction(1, 2)
 print(str(fraction1))  # 1/2
