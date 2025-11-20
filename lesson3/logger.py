4f0450a6eaa97a184aa6110095eda1f0a49c3950
Рыбина Лидия <lidiar@rambler.ru>
2025-11-20 20:17:41 +0500

    home work _ lesson 3 _ logger

--- lesson3/logger.py
+++ lesson3/logger.py
@@ -8,7 +8,12 @@
 
 
 class Logger:
-    pass
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __call__(self, message):
+        with open(self.filename, 'a') as f:
+            f.write(message+'\n')
 
 
 # код для проверки 
